f265d4dbbd0e71675d0c39950f83c01b0fde38d7
MarceloAdan73 <112431764+MarceloAdan73@users.noreply.github.com>
2023-12-11 21:56:05 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base :"/Calculator-React-Vite/",
+  base :"https://MarceloAdan73.github.io/Calculator-React-Vite/",
 });
